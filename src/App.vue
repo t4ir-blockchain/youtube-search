fa59bbd2e772666a5847a74ddac9425527b7bf19
John Kang <hphk.john@gmail.com>
2020-06-15 15:41:49 +0900

    Applied youtube api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Youtube Search</h1>
+    <SearchBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchBar
   }
 }
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <input v-model="userInput" @keypress.enter="input" type="text">
+    <ul>
+      <li v-for="result in results" :key="result.id.videoId">
+        {{ result.snippet.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'SearchBar',
+  data() {
+    return {
+      userInput: '',
+      results: []
+    }
+  },
+  methods: {
+    input() {
+      // 1. 입력된 검색어를 가지고,
+      const baseURL = 'https://www.googleapis.com/youtube/v3/search'
+      const API_KEY = '본인의 API_KEY'
+
+      // 2. Youtube API에 요청을 보내어
+      axios.get(baseURL, {
+        params: {
+          // key, part, q
+          key: API_KEY,
+          part: 'snippet',
+          type: 'video',
+          q: this.userInput
+        }
+      })
+        .then(response => {
+          console.log(response.data.items)
+          this.results = response.data.items
+        })
+      // 3. 검색어로 검색한 결과를 가져옴
+      console.log('검색어 입력 되었음')
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
